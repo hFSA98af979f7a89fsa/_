d1420d096786dccc857411d91a665ab88f02fa63
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-05 11:16:57 -0700

    Update a.lua

--- .github/workflows/a.lua
+++ .github/workflows/a.lua
@@ -1,2 +1,3 @@
 
 game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/scentedcondo'
+game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/scentedcondo'
