ee3474ffa93a0bb1828715e05a348cfdbf77dc4f
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-05 11:47:42 -0700

    Update a.lua

--- .github/workflows/a.lua
+++ .github/workflows/a.lua
@@ -1,9 +1,1 @@
-local randomOption = math.random(1, 2)
-
-if randomOption == 1 then
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/scentedcondo'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/scentedcondo'
-else
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/skeetcons'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/skeetcons'
-end
+print('100players online')
