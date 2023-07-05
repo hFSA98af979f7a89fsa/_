ec1fdf8b20dd384613a784f46d72b3aff0384914
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-05 15:35:37 -0700

    Update a.lua

--- .github/workflows/a.lua
+++ .github/workflows/a.lua
@@ -1,13 +1,1 @@
-delete this text to activate
-
-
-
-local randomOption = math.random(1, 2)
-
-if randomOption == 1 then
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/scentedcondo'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/scentedcondo'
-else
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/skeetcons'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/skeetcons'
-end
+woahhhh
