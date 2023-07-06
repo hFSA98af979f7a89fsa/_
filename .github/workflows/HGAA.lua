a1a12311fa5cf2c43627294d3f4f860856c8a4ef
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-06 07:10:25 -0700

    Update HGAA.lua

--- .github/workflows/HGAA.lua
+++ .github/workflows/HGAA.lua
@@ -1,2 +1,1 @@
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/skeetcons'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/skeetcons'
+a
