e784eac91a36684e31398b0b00b702400fe62b98
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-09 15:44:25 +0300

    Update HGAA.lua

--- .github/workflows/HGAA.lua
+++ .github/workflows/HGAA.lua
@@ -1,13 +1,13 @@
-
+wait(0.5)
 
 local randomOption = math.random(1, 2)
 
 if randomOption == 1 then
     game.ReplicatedStorage.DefaultChatSystemChatEvents.TextButton:Destroy()
     game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/scentedcondo'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssaa.Text = 'gg/scentedcondo'
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/scentedcondo'
 else
     game.ReplicatedStorage.DefaultChatSystemChatEvents.TextButton:Destroy()
     game.ReplicatedStorage.DefaultChatSystemChatEvents.TextLabel.Text = 'gg/skeetcons'
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssaa.Text = 'gg/skeetcons'
+    game.ReplicatedStorage.DefaultChatSystemChatEvents.Frame.Draggable.ssa.Text = 'gg/skeetcons'
 end
