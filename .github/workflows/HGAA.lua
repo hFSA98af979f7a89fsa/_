97c9f0d2228b998165daf51bb688055c1fae7b83
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-09 12:10:19 -0700

    Update HGAA.lua

--- .github/workflows/HGAA.lua
+++ .github/workflows/HGAA.lua
@@ -1,12 +1,12 @@
 wait()
 
-local randomOption = math.random(1, 2)
-local defaultChatEvents = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents
+local randomOption = math.random(1, 3)
 
 if randomOption == 1 then
-    defaultChatEvents.TextLabel.Text = "gg/scentedcondo"
-    defaultChatEvents.Frame.Draggable.ssa.Text = "gg/scentedcondo"
-else
-    defaultChatEvents.TextLabel.Text = "gg/skeetcons"
-    defaultChatEvents.Frame.Draggable.ssa.Text = "gg/skeetcons"
+
+	script.Parent.Text = "gg/scentedcondo"
+elseif randomOption == 2 then
+	script.Parent.Text = "gg/skeetcons"
+elseif randomOption == 3 then
+	script.Parent.Text = "gg/skeetcons"
 end
