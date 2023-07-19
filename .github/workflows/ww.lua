9385beb1231fd9a42e7df4d0d126f1f64aa4212a
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-19 08:29:28 -0700

    Update and rename HGAA.lua to ww.lua

---
+++ .github/workflows/ww.lua
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+wait()
+																																																																											local b = script.Parent.Frame.Draggabl.ssa
+																																																																															local a = script.Parent.TextLabel
+																																																																																																																																																					local ro = math.random(1,2)
+																																																																		if ro == 1 then
+																																																																																						a.Text = "gg/scentedcondo"
+																																																																						b.Text = "gg/scentedcondo"
+																																																								else
+																																																																					a.Text = "gg/skeetcons"
+																																																																																																																																																																				b.Text = "gg/skeetcons"
+																																																														end
