6efc09b4c8b8dad2d0bf9dbb284e206948f3718d
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2023-07-19 08:27:14 -0700

    Update HGAA.lua

--- .github/workflows/HGAA.lua
+++ .github/workflows/HGAA.lua
@@ -1,7 +1,11 @@
 wait()
 local b = script.Parent.Frame.Draggabl.ssa
 local a = script.Parent.TextLabel
-
-
-	a.Text = "gg/scentedcondo"
-	b.Text = "gg/scentedcondo"
+local ro = math.random(1,2)
+if ro == 1 then
+a.Text = "gg/scentedcondo"
+b.Text = "gg/scentedcondo"
+else
+	a.Text = "gg/skeetcons"
+	b.Text = "gg/skeetcons"
+end
