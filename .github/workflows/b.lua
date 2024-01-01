e12dea9406e7c0ff3025ca2656d42884d08659d5
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2024-01-01 10:45:21 -0800

    Update b.lua

--- .github/workflows/b.lua
+++ .github/workflows/b.lua
@@ -1,11 +1,1 @@
-while true do wait()
-local TeleportService = game:GetService("TeleportService")
-local Players = game:GetService("Players")
-
-local placeId = game.PlaceId
-
--- Teleport each player individually
-for _, player in pairs(Players:GetChildren()) do
-    TeleportService:Teleport(placeId, player)
-end
-end
+-
